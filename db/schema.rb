9cc077f93efa5fee95f350696c1bbcc0c3e5578d
Cameron Tong <cameron.tong2b@gmail.com>
2021-08-22 17:30:22 +0000

    finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 2021_08_20_212841) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password"
+    t.string "password_confirmation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
