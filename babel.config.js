d334b32430baae2c9d0fcb1174934d27c5679de6
Cameron Tong <cameron.tong2b@gmail.com>
2021-08-23 21:08:53 +0000

    revert config changes

--- babel.config.js
+++ babel.config.js
@@ -44,9 +44,9 @@ module.exports = function(api) {
       [
         '@babel/plugin-proposal-class-properties',
         {
-          loose: true
+          loose: true 
         }
-      ],["@babel/plugin-proposal-private-methods", { "loose": true }],
+      ],
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
